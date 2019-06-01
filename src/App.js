8d0e76c704a1c2a331d02d6b59833a52fd051e07
Monica Powell <monica@aboutmonica.com>
2019-06-01 19:56:08 -0400

    MVP lorem ipsum generator

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { Component } from "react";
+import { Footer } from "./Footer";
+import "./App.css";
+import parse from "html-react-parser";
 
-class LambdaDemo extends Component {
+class LambdaCall extends Component {
   constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+    super(props);
+    this.state = { loading: false, msg: null };
   }
 
   handleClick = api => e => {
-    e.preventDefault()
+    e.preventDefault();
 
-    this.setState({ loading: true })
+    this.setState({ loading: true });
     fetch("/.netlify/functions/" + api)
       .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+      .then(json => this.setState({ loading: false, msg: json.msg }));
+  };
 
   render() {
-    const { loading, msg } = this.state
+    const { loading, msg } = this.state;
 
     return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
+      <>
+        <p>
+          <button
+            onClick={this.handleClick("generate-lorem-ipsum")}
+            className="button"
+          >
+            {loading ? "Loading..." : "Generate Lorem Ipsum"}
+          </button>
+          <br />
+          {console.log(msg)}
+          {msg && parse(msg)}
+        </p>
+      </>
+    );
   }
 }
 
@@ -36,15 +44,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
+          <h1>
+            <span id="grey-title">Grey's</span> Anatomy <br /> Lorem Ipsum
+            Generator
+          </h1>
+          <LambdaCall />
         </header>
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/lambda/generate-lorem-ipsum.js
@@ -0,0 +1,67 @@
+import { WORDS } from "./words";
+export function handler(event, context, callback) {
+  const { queryStringParameters } = event;
+  const { paragraphs = 0, words = 0 } = queryStringParameters;
+  console.log("queryStringParameters", event.queryStringParameters);
+  console.log("p", paragraphs, "w", words);
+
+  let isParagraph = Boolean(paragraphs);
+  let count;
+
+  if (paragraphs > 1) {
+    count = paragraphs;
+  } else if (words > 0) {
+    count = words;
+  } else {
+    isParagraph = true;
+    count = 4;
+  }
+
+  let response;
+  try {
+    response = generateLoremIpsum(isParagraph, count).join(" ");
+  } catch (error) {
+    console.log(error);
+  }
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ msg: response })
+  });
+}
+
+export function generateLoremIpsum(isParagraph, count) {
+  if (isParagraph) {
+    console.log(`Trying to construct ${count} paragraphs`);
+    return generateParagraphs(count);
+  } else {
+    console.log(`Trying to return ${count} words`);
+    console.log([...new Set(WORDS)]);
+    return generateWords(count);
+  }
+}
+
+export function generateWords(wordCount) {
+  let words = [];
+  console.log(wordCount);
+
+  for (var i = 0; i < wordCount; i++) {
+    console.log("i", i);
+    words.push(WORDS[getRandomInt()]);
+  }
+  const formattedWords = `<p>${words.join(" ")}</p>`;
+
+  return formattedWords;
+}
+
+export function generateParagraphs(paragraphCount) {
+  let paragraphs = [];
+  for (var i = 0; i < paragraphCount; i++) {
+    paragraphs.push(generateWords(50));
+  }
+  return paragraphs;
+}
+
+export function getRandomInt() {
+  return Math.floor(Math.random() * Math.floor(WORDS.length));
+}
