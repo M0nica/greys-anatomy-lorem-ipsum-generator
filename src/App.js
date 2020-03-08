359663c74d606641a129360b310e33d8cedef315
Monica Powell <monica@aboutmonica.com>
2020-03-08 18:22:37 -0400

    convert to hooks 🏴‍☠️

--- src/App.js
+++ src/App.js
@@ -1,40 +1,35 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Footer } from "./Footer";
 import "./App.css";
 import parse from "html-react-parser";
 
-class LambdaCall extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false, msg: null };
-  }
+function Quotes() {
+  const [quotes, setQuotes] = useState({ loading: false, msg: null });
+  const { loading, msg } = quotes;
+  const api = "generate-lorem-ipsum";
 
-  handleClick = api => e => {
-    e.preventDefault();
+  function handleClick(event) {
+    event.preventDefault();
 
-    this.setState({ loading: true });
+    setQuotes({ ...quotes, loading: true });
     fetch("/.netlify/functions/" + api)
       .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }));
-  };
-
-  render() {
-    const { loading, msg } = this.state;
-
-    return (
-      <>
-        <p>
-          <button
-            onClick={this.handleClick("generate-lorem-ipsum")}
-            className="button"
-          >
-            {loading ? "Loading..." : "Generate Lorem Ipsum"}
-          </button>
-        </p>
-        {msg && parse(msg)}
-      </>
-    );
+      .then(json => setQuotes({ ...quotes, msg: json.msg }))
+      .catch(err => console.log(err))
+      .finally(setQuotes({ ...quotes, loading: false }));
   }
+
+  return (
+    <>
+      <p>
+        <button onClick={handleClick} className="button">
+          {loading ? "Loading..." : "Generate Lorem Ipsum"}
+          {console.log("STATE", quotes)}
+        </button>
+      </p>
+      {msg && parse(msg)}
+    </>
+  );
 }
 
 class App extends Component {
@@ -46,7 +41,7 @@ class App extends Component {
             <span id="grey-title">Grey's</span> Anatomy <br /> Lorem Ipsum
             Generator
           </h1>
-          <LambdaCall />
+          <Quotes />
         </header>
         <Footer />
       </div>
